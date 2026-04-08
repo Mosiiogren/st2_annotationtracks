ae4992594dca5b6752787d6d406362d083de9a68
Moa Ögren <moa.ogren@tegnet.se>
2026-04-08 12:31:24 +0200

    Update gens_update.sh

--- actions/gens_update.sh
+++ actions/gens_update.sh
@@ -4,8 +4,10 @@ set -e
 
 DOCKERCOMMAND=$1
 MONGOCOMMAND=$2
+USERNAME=$3
+PASSWORD=$4
 
-docker compose -f docker-compose.yaml ${DOCKERCOMMAND}
+docker compose -f docker-compose.yaml ${DOCKERCOMMAND} -u ${USERNAME} -p ${PASSWORD}
 
 echo "Working #1"
 

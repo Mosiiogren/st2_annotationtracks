bc4ebdaf1ee017bd5cff5057b0a54b220fd46be8
Moa Ögren <moa.ogren@tegnet.se>
2026-04-09 15:28:33 +0200

    Update copy_to_gens.sh

--- actions/copy_to_gens.sh
+++ actions/copy_to_gens.sh
@@ -6,6 +6,6 @@ ANNOTATIONTRACKS=$1
 GENSFOLDER=$2
 
 
-docker compose -f docker-compose.yaml docker cp ${ANNOTATIONTRACKS} gens:${GENSFOLDER}
+docker -f docker-compose.yaml cp ${ANNOTATIONTRACKS} gens:${GENSFOLDER}
 
 echo "Copy to Gens is Working"

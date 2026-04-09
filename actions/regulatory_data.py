aa9d25637c2361035095117302dc0ee88283c1c5
Moa Ögren <moa.ogren@tegnet.se>
2026-04-09 14:59:19 +0200

    Update regulatory_data.py

--- actions/regulatory_data.py
+++ actions/regulatory_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from st2common.runners.base_action import Action
 
-class VariantData (Action):
+class RegulatoryData (Action):
 
     def run (self, regulatoryfile):
         print ("Hello from regulatory_data")

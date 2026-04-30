8f5d1b1873d7c16c204d74d6d9dd36d998378420
Mosiiogren <moa.ogren@tegnet.se>
2026-04-30 13:40:26 +0200

    Feature: Included EMAR regions

--- actions/regulatory_data.py
+++ actions/regulatory_data.py
@@ -1,34 +1,47 @@
 import re
 import io
+import datetime
 import requests
+
 import pandas as pd
 import numpy as np
 
 from gzip import decompress
 from pathlib import Path
-
 from st2common.runners.base_action import Action
 
 
 class RegulatoryData(Action):
 
     def run(
-        self, regulatoryfileurl: str, outputfileregulatory: str
+        self,
+        regulatoryfileurl: list[str],
+        outputfileregulatory: str,
+        columns: list[str],
+        attributes: list[str],
+        outputfileregulatorytracks: str,
+        filename: str,
     ) -> tuple[bool, str]:
 
-        if Path(outputfileregulatory).exists():
-            return (True, "Regulator file already exists!")
+        df = pd.DataFrame()
+        for url in regulatoryfileurl:
+            succeded, results = self.get_data(url, columns, attributes)
+            if not succeded:
+                return (False, results)
 
-        succeded, results = self.get_data(regulatoryfileurl)
-        if not succeded:
-            return (False, results)
-        df = self.filter_regulatoryfactor_data(results)
+            df = pd.concat([df, results])
 
-        df.to_json(outputfileregulatory, orient="records")
+        df = self.add_comments(df)
+        self.save_to_file(df, outputfileregulatory)
+        filename = self.create_annotationtrack_files(
+            df, outputfileregulatorytracks, filename
+        )
 
-        return (True, f"Saved regulatory data to {outputfileregulatory}")
+        return (True, filename)
 
-    def get_data(self, url: str) -> tuple[bool, pd.DataFrame | str]:
+    def get_data(
+        self, url: str, columns: list[str], attributes: list[str]
+    ) -> tuple[bool, pd.DataFrame | str]:
         """
         Function for retrieving data from request
         """
@@ -50,19 +63,12 @@ class RegulatoryData(Action):
             dtype=str,
         )
 
-        df.columns = columns = [
-            "chromosome",
-            "source",
-            "Type",
-            "start",
-            "end",
-            "col6",
-            "col7",
-            "col8",
-            "col9",
-        ]
+        df.columns = columns
+
         df = self.get_attributes(
-            df=df, attributes=["ID", "gene_id"], last_column="col9"
+            df=df,
+            attributes=attributes,
+            last_column=columns[-1],
         )
 
         return (True, df)
@@ -79,22 +85,86 @@ class RegulatoryData(Action):
                 lambda x: (
                     re.findall(rf"{attribute}=([^;]*)", x)[0]
                     if f"{attribute}=" in x
-                    else np.nan
+                    else "-"
                 )
             )
+            df[attribute] = df[attribute].astype("str")
+            df.loc[df[attribute].isna(), attribute] = "-"
 
         df.drop(last_column, axis=1, inplace=True)
 
         return df
 
-    def filter_regulatoryfactor_data(self, df: pd.DataFrame) -> pd.DataFrame:
+    def add_comments(self, df: pd.DataFrame) -> pd.DataFrame:
         """
-        Function for filtering the data into the correct format
+        Function for adding a column consisting of comments
+        Comments are visually separate in Gens by ;
         """
+        df = df.astype(str)
+
+        df["comments"] = (
+            "Ensembl"
+            + ";"
+            + "Regulatory element: "
+            + df["Type"]
+            + ";"
+            + "ID: "
+            + df["ID"]
+            + ";"
+            + "Extended start: "
+            + df["extended_start"]
+            + ";"
+            + "Extended_end: "
+            + df["extended_end"]
+            + ";"
+            + "Gene ID: "
+            + df["gene_id"]
+            + ";"
+            + "Gene name: "
+            + df["gene_name"]
+            + ";"
+            + "Gene biotype: "
+            + df["gene_biotype"]
+            + ";"
+            + "Source: "
+            + df["source"]
+            + ";"
+            + "Track created at: "
+            + datetime.datetime.now().strftime("%c")
+        )
+
+        return df
+
+    def save_to_file(self, df: pd.DataFrame, outputfile):
+        """
+        Function for saving data to json file
+        """
+        df = df[df["Type"] != "open_chromatin_region"]
+        df.to_json(outputfile, orient="records")
+
+    def create_annotationtrack_files(
+        self, df: pd.DataFrame, outputfolder: str, filename: str
+    ) -> list[str]:
+
+        df = df.drop(
+            df.columns.difference(
+                [
+                    "chromosome",
+                    "start",
+                    "end",
+                    "comments",
+                ]
+            ),
+            axis=1,
+        )
 
         df["start"] = df["start"].astype("int")
         df["end"] = df["end"].astype("int")
 
-        df = df[df["Type"] != "open_chromatin_region"]
+        df.to_csv(
+            (outputfolder + filename),
+            sep="\t",
+            index=False,
+        )
 
-        return df
+        return [filename]

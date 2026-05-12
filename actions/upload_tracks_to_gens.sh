6a431eadbb061fa7b6353bbff8933406f8412575
Mosiiogren <moa.ogren@tegnet.se>
2026-05-12 09:21:32 +0200

    Refactors: Merge files together

--- actions/upload_tracks_to_gens.sh
+++ actions/upload_tracks_to_gens.sh
@@ -9,7 +9,8 @@ IFS="," read -a array <<< "${ANNOTATIONTRACKS}"
 
 for file in "${array[@]}"
 do
-    echo "Copying $file to Gens container"
+    echo "Copying $file to Gens"
     docker compose -f docker-compose.yaml cp "${OUTPUTFOLDER}$file" gens:/data/wgs/annotationtracks/
-    
+    echo "Uploading $file to Gens"
+    docker compose -f docker-compose.yaml exec gens gens load annotations -b 38 -f /data/wgs/annotationtracks/$file
 done

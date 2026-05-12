328b5949ecc02a77b5c7800894c32c28ea775239
Mosiiogren <moa.ogren@tegnet.se>
2026-05-12 09:18:44 +0200

    Feature: Started to include ACMG genes

---
+++ actions/acmg_genes.py
@@ -0,0 +1,5 @@
+# Add ACMG genes
+# https://ftp.ncbi.nlm.nih.gov/pub/dbVar/sandbox/annotation/GRCh38/ACMG_genes.gff.gz
+
+
+# https://ftp.ensembl.org/pub/release-114/variation/PhenotypeOrthologous/PhenotypesOrthologous_homo_sapiens_112_GRCh38.gff3.gz
